814bedef3663c5ce841e962c15bae3b74a542c98
= <=>
2017-03-29 11:58:45 -0700

    Q

--- eig_max_pcolor.m
+++ eig_max_pcolor.m
@@ -1,31 +1,23 @@
-% Monodromy Matrix Derivation
-% Author: Yanran Ding
-
+k = 50
+% k = linspace(0,4,10);
+% T_st = linspace(0.05,0.2,10);
+eig_max = zeros(size(k));
 M = 0.675;
-len_k = 10;
-len_Tst = 10;
-k_span = linspace(0,4,len_k);
-Tst_span = linspace(0.05,0.5,len_Tst);
-[k, Tst] = meshgrid(k_span,Tst_span);
-eig_max = zeros(len_k,len_Tst);
+Tst = 0.18;
+Tflt = 0.6;
 
+% for i = 1:length(k)
+    % jump dynamics: dq = f1 = [dz;ddz] = [dz;Fz/M - g + k/M*(dz_d-dz)]
+    % df1_dq = [0 1;0 -k/M] = dQ1_dt
+    % flight dynamics: dq = f2 = [dz;ddz] = [dz;-g]
+    % df2_dq = [0 1;0 0] = dQ2_dt
+    % Q = expm(dQ1_dt*Tst)*expm(dQ2_dt*Tflt) <-- The monodromy matrix
+    Q = expm([0 1;0 0]*Tflt)*expm([0 1;0 -k/M]*Tst);
+    [eigVec, eigVal] = eig(Q)
+%     fprintf('eigenvales are: %.2f, %.2f \n',eigVal(1,1),eigVal(2,2))
+%     fprintf('eigenvectors are: [%.2f;%.2f],[%.2f,%.2f] \n',eigVec(:,1),eigVec(:,2))
+% end
 
-for ii = 1:len_k
-    for jj = 1:len_Tst
-        % jump dynamics: dq = f1 = [dz;ddz] = [dz;Fz/M - g + k/M*(dz_d-dz)]
-        % df1_dq = [0 1;0 -k/M] = dQ1_dt
-        % Q1 = expm([0 1;0 -k/M]*Tst) <-- Monodromy at takeoff
-        % flight dynamics: dq = f2 = [dz;ddz] = [dz;-g]
-        % df2_dq = [0 1;0 0] = dQ2_dt
-        % Q2 = expm([0 1;0 0]*Tfl) <-- Monodromy at the apex from takeoff
-        % Q = Q2*Q1
-%         Q = expm([0 1;0 0])*expm([0 1;0 -k(i)/M]);
-        Qapex = expm([0 1;0 0])*expm([0 1;0 -k(ii)/M]*Tst(jj));
-        [eigVec, eigVal] = eig(Qapex);
-        fprintf('eigenvales are: %.2f, %.2f \n',eigVal(1,1),eigVal(2,2))
-        fprintf('eigenvectors are: [%.2f;%.2f],[%.2f,%.2f] \n',eigVec(:,1),eigVec(:,2))
-    end
-end
 % plot(k,eig_max)
 % xlabel('k, feedback gain')
 % ylabel('maximum eigen-value for Monodromy Matrix')
